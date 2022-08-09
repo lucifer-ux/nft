f68533457aa3d3bf186ab5544cfcb730eed42d1f
Bash <contact.qmicon@gmail.com>
2022-08-09 14:51:51 +0530

    added FAQ

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -6,66 +6,51 @@ const Faq = () => {
     <h1>FAQ</h1>
 
 <details itemScope itemProp="mainEntity" itemType="https://schema.org/Question">
-  <summary itemProp="name">A question about something</summary>
+  <summary itemProp="name">What is a Chad Card?</summary>
     <div className="details-expanded" itemScope itemProp="acceptedAnswer" itemType="https://schema.org/Answer">
-    <p>The answer about this has some points to make:</p>
-    <ol>
-    <li>Do this and then</li>
-    <li>The next step</li>
-    <li>Almost there</li>
-    <li>Yes, this is it</li>
-    <li>There is no step 5</li>
-    </ol>
+    <p>The Chad Card is a set of 696 unique tokens of the world's first Refer to Earn NFT, with a chance to earn up to 50% on each referral. 
+    </p><p> The owners of the Elite Chad Card and the Alpha Chad Card would have the discretion to recommend individuals for a limited number of NFT passes to enter Dream Date World.
+    </p>
   </div>
 </details>
 
 <details itemScope itemProp="mainEntity" itemType="https://schema.org/Question">
-  <summary itemProp="name">Summary</summary>
+  <summary itemProp="name">What is R2E?</summary>
   <div className="details-expanded" itemScope itemProp="acceptedAnswer" itemType="https://schema.org/Answer">
-    <h2>Test headline</h2>
-    <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-    <li>5</li>
-    </ul>
+    <p>R2E, as the name implies, is a <u><b>Refer To Earn</b></u> eco-system in which Chad Card holders can recommend individuals for a limited number of NFT passes to enter Dream Date World.</p>
   </div>
 </details>
 <details itemScope itemProp="mainEntity" itemType="https://schema.org/Question">
-  <summary itemProp="name">Summary</summary>
+  <summary itemProp="name">What is Elite R2E (Elite Chad Card) ?</summary>
     <div className="details-expanded" itemScope itemProp="acceptedAnswer" itemType="https://schema.org/Answer">
-    <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-    <li>5</li>
-    </ul>
+    <p>Elite Chad Card members are limited by the total number of 20 NFT Token.</p>
+    <p> Elite Chad Card Holders would have the superpowers to recommend individuals for a limited number of NFT passes to enter Dream Date World.</p>
   </div>
 </details>
 <details itemScope itemProp="mainEntity" itemType="https://schema.org/Question">
-  <summary itemProp="name">Summary</summary>
+  <summary itemProp="name">How Elite R2E works?</summary>
     <div className="details-expanded" itemScope itemProp="acceptedAnswer" itemType="https://schema.org/Answer">
+    <h4>Elite Chad Holders have the R2E superpower which they can use in the following way:</h4>
     <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-    <li>5</li>
+      <li>Decide a minting price of the Alpha Chad</li>
+      <li>Generate Alpha R2E referral code</li>
+      <li>Send the Referral code to the recommended individual</li>
+      <li>The individual uses the Referral code to mint an Alpha Chad</li>
+      <li>Earn 50% of the minting price for every successful mint</li>
     </ul>
+    <p>All Elite Chad Holders retain their R2E superpower for the Future NFT Collections</p>
   </div>
 </details>
 <details itemScope itemProp="mainEntity" itemType="https://schema.org/Question">
-  <summary itemProp="name">Summary</summary>
+  <summary itemProp="name">What is an Alpha R2E (Alpha Chad Card) ?</summary>
     <div className="details-expanded" itemScope itemProp="acceptedAnswer" itemType="https://schema.org/Answer">
-    <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-    <li>5</li>
-    </ul>
+    <p>Alpha Chad Card owners limited only by a total number of 676, would have the ability to recommend individuals for a limited number of NFT passes to enter Dream Date World.</p>
+  </div>
+</details>
+<details itemScope itemProp="mainEntity" itemType="https://schema.org/Question">
+  <summary itemProp="name">How Alpha R2E works?</summary>
+    <div className="details-expanded" itemScope itemProp="acceptedAnswer" itemType="https://schema.org/Answer">
+    <p>Members of Alpha R2E determine the minting price of future NFT Tokens, then create a Referral code to send it to the recommend individuals, and on each sale they receive upto 50% of the minting price and the ability to recommend individuals for a limited number of NFT passes to enter Dream Date World.</p>
   </div>
 </details>
     </>
