04daf429e6dec00467455795a48ad1c4829a5e9f
Shashwat Shukla <shashwat.shukla@juspay.in>
2022-08-10 12:57:32 +0530

    ui fixes

--- src/components/MuteButton/muteButton.js
+++ src/components/MuteButton/muteButton.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import "./MuteButton.css"
 
 class MuteButton extends Component {
 
   render() {
     if (this.props.isMuted === true) {
       return (
-        <div onClick={this.props._toggleMuteButton}>
-          <FontAwesomeIcon icon="volume-mute" size="3x" />
+        <div onClick={this.props._toggleMuteButton} className='Volume_container'>
+          <FontAwesomeIcon icon="volume-mute" />
         </div>
       )
     }
     return ( 
       <div onClick={this.props._toggleMuteButton}>
-        <FontAwesomeIcon icon="volume-up" size="3x" />
+        <FontAwesomeIcon icon="volume-up"  />
       </div>
     )
   }
